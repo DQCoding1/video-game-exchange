1abee1ba0a75e10ee7518d6662ad5935dcaf315f
DQCoding1 <diegoabquintero@gmail.com>
2023-04-24 16:33:00 -0500

    creating html to login

--- src/views/login/Login.jsx
+++ src/views/login/Login.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <section>
+      <form>
+        <h1>Login</h1>
+        <label htmlFor="name">Name</label>
+        <input type="text" id='name' />
+        <label htmlFor="password">Password</label>
+        <input type="password" id='password' />
+        <input type="submit" name="" id="" value="Login" />
+      </form>
+    </section>
   )
 }
 
